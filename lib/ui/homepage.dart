da6a630a366e7c80bdbbf8fdb5142ea47a4acd3d
Rozaq Ingsoon <email.ingsoon@gmail.com>
2021-02-02 15:40:58 +0700

    pembuatan judul selesai

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tpaquiz/shared/warna.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +8,8 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          color : warnaBg,
-          padding: EdgeInsets.all(25),
+          color: warnaBg,
+          padding: EdgeInsets.all(35),
           child: Column(
             children: [
               Expanded(
@@ -20,13 +21,74 @@ class HomePage extends StatelessWidget {
                       Expanded(
                         flex: 2,
                         child: Container(
-                          color: Colors.amber,
+                          // color: Colors.amber,
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FittedBox(
+                                  fit: BoxFit.contain,
+                                  child: Text(
+                                    'TES',
+                                    style: GoogleFonts.fredokaOne(
+                                      textStyle: TextStyle(
+                                          fontSize: 60,
+                                          color: warnaTeksJudul,
+                                          // fontWeight: FontWeight.bold,
+                                          letterSpacing: 1),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.contain,
+                                  child: Text(
+                                    'POTENSI',
+                                    style: GoogleFonts.fredokaOne(
+                                      textStyle: TextStyle(
+                                          fontSize: 60,
+                                          color: warnaTeksJudul,
+                                          // fontWeight: FontWeight.bold,
+                                          letterSpacing: 1),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.contain,
+                                  child: Text(
+                                    'AKADEMIK',
+                                    style: GoogleFonts.fredokaOne(
+                                      textStyle: TextStyle(
+                                          fontSize: 60,
+                                          color: warnaTeksJudul,
+                                          // fontWeight: FontWeight.bold,
+                                          letterSpacing: 1),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                       Expanded(
                         flex: 1,
                         child: Container(
-                          color: Colors.cyanAccent,
+                          // color: Colors.cyanAccent,
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: Text(
+                              'Q',
+                              style: GoogleFonts.lobster(
+                                textStyle: TextStyle(
+                                    fontSize: 200,
+                                    color: warnaTeksJudul,
+                                    // fontWeight: FontWeight.bold,
+                                    letterSpacing: 1),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ],
