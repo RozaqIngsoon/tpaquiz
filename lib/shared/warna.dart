deb990b3e8c04bfae5f854781719fe23b8264308
Rozaq Ingsoon <email.ingsoon@gmail.com>
2021-02-02 15:26:46 +0700

    + 1 warna, atur container home page

---
+++ lib/shared/warna.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color warna1 = Color(0xffD4C8C8);
--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:tpaquiz/shared/warna.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('tes lagi lagi'),),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          color : warna1,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
