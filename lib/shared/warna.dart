3b24e8c6fd7274130367e67be473e1b7db38b529
Rozaq Ingsoon <email.ingsoon@gmail.com>
2021-02-03 11:49:39 +0700

    tambah warna 4 tombol

--- lib/shared/warna.dart
+++ lib/shared/warna.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 
 Color warnaBg = Color(0xffD4C8C8);
-Color warnaJudul = Color(0xff707070);
+Color warnaTeksHome = Color(0xff707070);
+Color warnaTombolHome1 = Color(0xffA4E9FB);
+Color warnaTombolHome2 = Color(0xff95FCCF);
+Color warnaTombolHome3 = Color(0xffF9F987);
+Color warnaTombolHome4 = Color(0xffFCB7B7);
