a04e43f5d94625500a152d7352587c63d5764704
Rozaq Ingsoon <email.ingsoon@gmail.com>
2021-02-05 12:40:51 +0700

    5/2/2021 - ui page mulai sudah selesai. Project ditinggal dulu. Mbangun aplikasi SA78

--- lib/provider/mulai_screen.dart
+++ lib/provider/mulai_screen.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/foundation.dart';
 
-class MulaiScreen with ChangeNotifier{
+class MulaiScreen with ChangeNotifier {
+  bool _tipeTanya = true; // tipe teks = true, tipe gambar = false
+  bool get tipeTanya => _tipeTanya;
+  set tipeTanya(bool value) {
+    _tipeTanya = value;
+    notifyListeners();
+  }
+
+  bool _tipeJawab = true; // tipe teks = true, tipe gambar = false
+  bool get tipeJawab => _tipeJawab;
+  set tipeJawab(bool value) {
+    _tipeJawab = value;
+    notifyListeners();
+  }
+
+  bool _tipePenjelasan = true; // tipe teks = true, tipe gambar = false
+  bool get tipePenjelasan => _tipePenjelasan;
+  set tipePenjelasan(bool value) {
+    _tipePenjelasan = value;
+    notifyListeners();
+  }
 
 }
--- lib/ui/mulai.dart
+++ lib/ui/mulai.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:provider/provider.dart';
+import 'package:tpaquiz/provider/mulai_screen.dart';
 import 'package:tpaquiz/shared/warna.dart';
 
 class Mulai extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var mulaiScreen = Provider.of<MulaiScreen>(context);
 
-    Widget menuLewati (){
+    Widget menuLewati() {
       return Container(
         // color: Colors.cyanAccent,
         child: Row(
@@ -87,31 +90,20 @@ class Mulai extends StatelessWidget {
       return Align(
         alignment: Alignment.topCenter,
         child: Transform.translate(
-          offset: Offset(0, -(MediaQuery
-              .of(context)
-              .size
-              .height / 18)),
+          offset: Offset(0, -(MediaQuery.of(context).size.height / 18)),
           child: Container(
             decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: warnaPersenIndikator1,
                 boxShadow: [
                   BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0, 1),
-                      blurRadius: 6)
+                      color: Colors.grey, offset: Offset(0, 1), blurRadius: 6)
                 ]),
             child: Transform.translate(
               offset: Offset(0, 0),
               child: CircularPercentIndicator(
-                radius: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 9,
-                lineWidth: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 150,
+                radius: MediaQuery.of(context).size.height / 9,
+                lineWidth: MediaQuery.of(context).size.height / 150,
                 backgroundColor: warnaPersenIndikator2,
                 progressColor: warnaPersenIndikator3,
                 percent: 0.5,
@@ -122,8 +114,8 @@ class Mulai extends StatelessWidget {
                     padding: EdgeInsets.all(15),
                     child: Text(
                       '60',
-                      style: TextStyle(
-                          fontSize: 35, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
@@ -134,9 +126,10 @@ class Mulai extends StatelessWidget {
       );
     }
 
-    Widget kemampuan_nmrsoal() {
+    Widget kemampuan_nmrsoal({String jenis, String tipe, int soalke}) {
       return Container(
-        constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height/18),
+        constraints:
+            BoxConstraints(minHeight: MediaQuery.of(context).size.height / 18),
         padding: EdgeInsets.only(top: 10, left: 10, right: 10),
         child: Row(
           children: [
@@ -147,8 +140,14 @@ class Mulai extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('NUMERIK', style: TextStyle(color: Colors.red),),
-                    Text ('Deret Angka', style: TextStyle(color: Colors.blue, fontSize: 10),)
+                    Text(
+                      jenis,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    Text(
+                      '( $tipe )',
+                      style: TextStyle(color: Colors.blue, fontSize: 10),
+                    )
                   ],
                 ),
               ),
@@ -156,15 +155,15 @@ class Mulai extends StatelessWidget {
             Expanded(
               flex: 2,
               child: Container(
-                // color: Colors.white,
-              ),
+                  // color: Colors.white,
+                  ),
             ),
             Expanded(
               flex: 3,
               child: Container(
                 // color: Colors.red,
                 alignment: Alignment.centerRight,
-                child: Text('2/30'),
+                child: Text('$soalke/30'),
               ),
             ),
           ],
@@ -172,110 +171,201 @@ class Mulai extends StatelessWidget {
       );
     }
 
-    Widget perintah_soal (){
+    Widget perintah_soal() {
       return Container(
-        padding: EdgeInsets.only(top: 10, bottom: 10),
-        // color: Colors.brown,
-          child: Center(child: Text('JAwab pertanyann pertanyaan ini dengan benar ! engan benar !\n  ini dengan benar ', textAlign: TextAlign.center,)));
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          // color: Colors.brown,
+          child: Center(
+              child: Text(
+            'JAwab pertanyann pertanyaan ini dengan benar ! ',
+            textAlign: TextAlign.center,
+          )));
     }
+
+    Widget pertanyaan() {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10)),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey, offset: Offset(0, 2), blurRadius: 10)
+            ]),
+        child: mulaiScreen.tipeTanya ? Text(
+          'pertanyaan ',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ) :
+        Image.network('https://bit.ly/3tuze63'),
+      );
+    }
+
+    Widget tombolJawaban({String pilihan, String isiPilihan}) {
+      return Container(
+        child: RaisedButton(
+          color: Colors.white,
+          elevation: 5,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Container(
+            padding: EdgeInsets.all(5),
+            child: Row(
+              children: [
+                Text(
+                  '$pilihan.',
+                  style: TextStyle(color: Colors.redAccent),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Flexible(child: Text(isiPilihan))
+              ],
+            ),
+          ),
+          onPressed: () {},
+        ),
+      );
+    }
+
+    Widget timer60mnt() {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: Transform.translate(
+          offset: Offset(0, 0),
+          child: Container(
+            color: warnaPersenIndikator1,
+            child: LinearProgressIndicator(
+              value: 0.5,
+            ),
+          ),
+        ),
+      );
+    }
+
+    ;
 
     return WillPopScope(
         onWillPop: () async => false,
         child: SafeArea(
           child: Scaffold(
-            body: Container(
-              color: warnaBg,
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .height,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
-              padding: EdgeInsets.fromLTRB(25, 10, 25, 5),
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: menuLewati (),
-                  ),
-                  Expanded(
-                    flex: 9,
-                    child: Container(
-                      // color: Colors.amberAccent,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey,
-                              offset: Offset(0, 2),
-                              blurRadius: 10)
-                        ]
+            body: Stack(
+              children: [
+                Container(
+                  color: warnaBg,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.fromLTRB(25, 10, 25, 5),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: menuLewati(),
                       ),
-                      child: Stack(
-                        children: [
-                          Column(
+                      Expanded(
+                        flex: 9,
+                        child: Container(
+                          child: Stack(
                             children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(topRight: Radius.circular(10), topLeft: Radius.circular(10)),
-                                    color: Colors.white,
-                                ),
-                                child: Column(
-                                  children: [
-                                    kemampuan_nmrsoal(),
-                                    perintah_soal(),
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  padding: EdgeInsets.all(10),
-                                  width: MediaQuery.of(context).size.width,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
-                                      color: Colors.white,
-                                  ),
-                                  child: SingleChildScrollView(
+                              Column(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(10),
+                                            topLeft: Radius.circular(10)),
+                                        // borderRadius: BorderRadius.circular(10),
+                                        color: Colors.white,
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: Colors.grey,
+                                              offset: Offset(0, 2),
+                                              blurRadius: 10)
+                                        ]),
                                     child: Column(
-                                      mainAxisSize: MainAxisSize.max,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-                                        Text('dddd', style: TextStyle(fontSize: 35),),
-
+                                        kemampuan_nmrsoal(
+                                            jenis: 'NUMERIK',
+                                            tipe: 'Deret Angka',
+                                            soalke: 5),
+                                        perintah_soal(),
                                       ],
                                     ),
                                   ),
-                                ),
+                                  Expanded(
+                                    child: Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      color: warnaBg,
+                                      child: SingleChildScrollView(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            pertanyaan(),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            tombolJawaban(
+                                                pilihan: 'A.',
+                                                isiPilihan:
+                                                    'bklla aedfn asf '),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            tombolJawaban(
+                                                pilihan: 'B.',
+                                                isiPilihan:
+                                                    'bklla aedfn asf  dfaf sadfa safda adf'),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            tombolJawaban(
+                                                pilihan: 'C.',
+                                                isiPilihan:
+                                                    'bklla aedfn as'),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            tombolJawaban(
+                                                pilihan: 'D.',
+                                                isiPilihan:
+                                                    'bklla aedfn asf '),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            tombolJawaban(
+                                                pilihan: 'E.',
+                                                isiPilihan:
+                                                    'bklla aedfn asf  dfaf sadfa safda adfaf '),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Positioned.fill(
+                                child: timer30sc(),
                               ),
                             ],
                           ),
-                          Positioned.fill(
-                            child: timer30sc(),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Positioned.fill(
+                  child: timer60mnt(),
+                ),
+              ],
             ),
           ),
-        )
-    );
+        ));
   }
 }
